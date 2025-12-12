0d027133c80696736d44fc1a33fe84914f86a531
Vinicius Melo <122281546+ViniciusMMelo@users.noreply.github.com>
2025-12-12 17:06:53 +0000

    add create, read, update and search feature

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,14 +1,52 @@
-from fastapi import FastAPI
-from service import get_paginate, get_db
+from fastapi import APIRouter, FastAPI, HTTPException
+from service import get_db, add_species, search_species_full, update_species, get_specie_by_id
+from schemas import SpeciesCreate, SpeciesResponse
+from typing import Optional
 
-app = FastAPI()
+app = FastAPI(
+    title="Rest API com FastAPI",
+    version="1.0.0"
+)
 
-@app.get("/species")
-def return_species(start: int = None, end: int = None):
-    
-    if start is not None and end is not None:
-        page = get_paginate(start, end)
-        return page
-        
+app_route = APIRouter(tags=['Species'])
+
+@app_route.get("/species")
+def return_species(start: int = None, end: int = None, search: Optional[str] = None):
+    if search:
+        data = search_species_full(search)
     else:
-        return get_db()
+        data = get_db()
+
+    if start is not None and end is not None:
+        final_result = data[start:end]
+        return final_result
+    
+    return data
+
+@app_route.get("/species/{id_specie}", response_model=SpeciesResponse)
+def return_species_by_id(id_specie: int):
+    result = get_specie_by_id(id_specie)
+
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="Espécie não encontrada")
+
+@app_route.post("/species", response_model=SpeciesResponse)
+def create_species_route(species: SpeciesCreate):
+    data_dict = species.dict()
+    new_specie = add_species(data_dict)
+    
+    return new_specie
+
+@app_route.put("/species/{id_specie}", response_model=SpeciesResponse)
+def update_species_route(id_specie: int, species: SpeciesCreate):
+    data_dict = species.dict()
+    result = update_species(id_specie, data_dict)
+    
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="Espécie não encontrada")
+
+app.include_router(app_route)
--- src/backend/service.py
+++ src/backend/service.py
@@ -6,16 +6,89 @@ def get_db():
 
     return data_json
 
+def save_db(data):
+    with open("../data/species.json", "w", encoding="utf-8") as file_json:
+        json.dump(data, file_json, indent=2, ensure_ascii=False)
 
-def get_paginate(start, end):
-    get_species = []
+# def get_paginate(start, end):
+#     get_species = []
 
-    for i, species in enumerate(get_db()):
-        if i >= start and i < end:
-            get_species.append(species)
+#     for i, species in enumerate(get_db()):
+#         if i >= start and i < end:
+#             get_species.append(species)
     
-        if i >= end:
-            break
+#         if i >= end:
+#             break
 
-    return get_species  
+#     return get_species  
 
+def get_specie_by_id(id_specie):
+    db = get_db()   
+    for specie in db:
+        if specie["id"] == id_specie:
+            return specie
+    return None 
+
+def add_species(new_specie_data):
+    db = get_db()
+
+    if db:
+        novo_id = max(item["id"] for item in db) + 1
+    else:
+        novo_id = 1
+
+    new_specie = {
+        "id": novo_id,
+        "commonName": new_specie_data["commonName"],
+        "scientificName": new_specie_data["scientificName"],
+        "biomes": new_specie_data["biomes"],
+        "description": new_specie_data["description"]
+    }
+
+    db.append(new_specie)
+    save_db(db)
+    
+    return new_specie
+
+def update_species(id_specie, data_update):
+    db = get_db()
+    isFound_specie = False
+
+    for index, specie in enumerate(db):
+        if specie["id"] == id_specie:
+            db[index].update(data_update)
+            db[index]["id"] = id_specie 
+            isFound_specie = True
+
+            break 
+
+    if isFound_specie:
+        save_db(db)
+
+        return db[index] 
+
+    else:
+        return None 
+
+
+def search_species_full(query):
+    db = get_db()
+    
+    if not query:
+        return db
+
+    queryLower = query.lower()
+
+    result = []
+    
+    for specie in db:
+        commonName_match = queryLower in specie.get("commonName", "").lower()
+        scientificName_match = queryLower in specie.get("scientificName", "").lower()
+        description_match = queryLower in specie.get("description", "").lower()
+        biomes_match = any(queryLower in b.lower() for b in specie.get("biomes", []))
+
+        # 3. Se for encontrado em QUALQUER um dos lugares, adiciona à lista
+        if commonName_match or scientificName_match or description_match or biomes_match:
+            result.append(specie)
+
+    return result
